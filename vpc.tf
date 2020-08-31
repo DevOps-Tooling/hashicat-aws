6b31958bbcf9817ea2e12c4c76a157e8d5d5a566
Krishan Kant <kant.krishan@bcg.com>
2020-08-31 08:24:49 +0000

    added module

--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,12 @@ module "vpc" {
   source  = "app.terraform.io/krishan-training/vpc/aws"
   version = "2.48.0"
   # insert required variables here
+  cidr_block = var.address_space
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "${var.prefix}-vpc"
+    Billable = "true"
+    Department = "DevOps"
+  
 }
